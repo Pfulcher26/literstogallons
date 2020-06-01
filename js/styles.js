b9c05cb8551d730268445a6bd0623e92a76d1cca
Payne Fulcher <pfulcher26@gmail.com>
2020-06-01 11:16:45 -0700

    Add alert result
    Co-authored-by: Peter Grimm <pagrimm@gmail.com>
    Co-authored-by: Ian Scott <chesnekov@gmail.com>

--- js/styles.js
+++ js/styles.js
@@ -1,3 +1,7 @@
 function gallonstoliters(gallons) {
 return parseInt((gallons*3.785412)); 
 };
+
+const number1 = parseInt(prompt("Enter a number:"));
+const result = gallonstoliters(number);
+alert(result);
